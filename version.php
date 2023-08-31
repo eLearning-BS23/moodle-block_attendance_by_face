5e490e11fa5453d5ee98690022ed26ab4f8235b9
Shadman Ahmed <72008371+shadman-ahmed-bs23@users.noreply.github.com>
2023-08-31 19:47:44 +0600

    Update version file with release version and maturity

--- version.php
+++ version.php
@@ -25,5 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_attendance_by_face';
+$plugin->release = '1.0.0';
 $plugin->version = 2023072406;
 $plugin->requires = 2020061500;
+$plugin->maturity = MATURITY_BETA;
